828aed4ffa4cb17af8526d6b8346f8f3ab4a9bd9
Leelaka Adassuriya <37642488+Leelaka@users.noreply.github.com>
2023-01-30 03:08:55 +1100

    make change to growl hook

--- src/growl.js
+++ src/growl.js
@@ -1,25 +1,34 @@
-import React from 'react'
+import React, { useEffect } from "react";
 
-import './growl.css'
+import "./growl.css";
 
 export const Growl = ({ active, message, onDismissed }) => (
   <div className={`growl${active ? " active" : ""}`}>
     {message}
     <div onClick={onDismissed} className="growl-close" />
   </div>
-)
+);
 
-export function useGrowl() {
-    // state of the growl
-    const [growlActive, setGrowlActive] = React.useState(false)
+export function useGrowl(dismissAfter = 3000) {
+  // state of the growl
+  const [growlActive, setGrowlActive] = React.useState(false);
 
-    return [
-        // the first arg is the state
-        growlActive, 
+  useEffect(() => {
+    if (growlActive) {
+      const timeout = setTimeout(() => {
+        setGrowlActive(false);
+      }, dismissAfter);
+      return () => clearTimeout(timeout);
+    }
+  }, [growlActive, dismissAfter]);
 
-        // the second arg is a fn that allows you to safely set its state
-        (active) => {
-            setGrowlActive(active)
-        },
-    ]
+  return [
+    // the first arg is the state
+    growlActive,
+
+    // the second arg is a fn that allows you to safely set its state
+    (active) => {
+      setGrowlActive(active);
+    },
+  ];
 }
